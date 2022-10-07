c39b0865ed47777196fa03c5efc5c068a9eb3a3a
villeyli <78146205+villeyli@users.noreply.github.com>
2022-10-07 13:11:41 +0300

    added new dao methods

--- src/main/java/group4/householdhero/model/Testmain.java
+++ src/main/java/group4/householdhero/model/Testmain.java
@@ -2,21 +2,23 @@ package group4.householdhero.model;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Testmain extends Application{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Model model = new Model();
         DataAccessObject dao = new DataAccessObject(model);
+        
 
-        Product product = model.createProduct(0, "maito", 0.99, LocalDate.parse("2022-10-22"), "maitotuote", 1, 1);
+        //Product product = model.createProduct(0, "maito", 0.99, LocalDate.parse("2022-10-22"), "maitotuote", 1, 1);
         try {
-            dao.getProductsInFridge();
+        	dao.checkBestBefore();
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             System.out.println(e);
         }
     }
